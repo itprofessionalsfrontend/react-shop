3f58d4ca2177040120c00bb1c7eb938880ccb417
MUSTAFA TOPÇU <mustafatopcu54@gmail.com>
2020-05-04 20:11:39 +0100

    React Context conversion

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Scrollbars } from "react-custom-scrollbars";
+import { ProductContextConsumer } from "../productContext.js";
 
 class Header extends Component {
   state = {
@@ -50,91 +50,96 @@ class Header extends Component {
   }
 
   render() {
-    console.log("header --->" + this.props.cartProducts.length);
-    let items = this.props.cartProducts.map((product) => {
-      return (
-        <li className="cart-item" key={product.id}>
-          <img
-            className="product-image"
-            alt="Product"
-            src={product.image}
-          ></img>
-          <div className="product-info">
-            <p className="product-name">{product.name}</p>
-            <p className="product-price">{product.price}</p>
-          </div>
-          <div className="product-total">
-            <p className="quantity">{product.quantity}</p>
-            <p className="amount">{product.price * product.quantity}</p>
-          </div>
-          <a
-            className="product-remove"
-            onClick={() => this.props.removeItem(product.id)}
-          >
-            x
-          </a>
-        </li>
-      );
-    });
+    // console.log("header --->" + this.props.cartProducts.length);
 
     return (
-      <header>
-        <div className="container">
-          <div className="brand">
-            <img
-              className="logo"
-              alt="Veggie Logo"
-              src="https://raw.githubusercontent.com/itprofessionalsfrontend/shop/master/assets/veggie.png"
-            ></img>
-          </div>
-          <div className="search">
-            <div className="search-form">
-              <input
-                type="search"
-                className="search-keyword"
-                placeholder="Search vegetables and fruits..."
-                onChange={this.props.search}
-              ></input>
+      <ProductContextConsumer>
+        {({ itemCount, totalPrice, cartProducts, removeProduct }) => (
+          <header>
+            <div className="container">
+              <div className="brand">
+                <img
+                  className="logo"
+                  alt="Veggie Logo"
+                  src="https://raw.githubusercontent.com/itprofessionalsfrontend/shop/master/assets/veggie.png"
+                ></img>
+              </div>
+              <div className="search">
+                <div className="search-form">
+                  <input
+                    type="search"
+                    className="search-keyword"
+                    placeholder="Search vegetables and fruits..."
+                    onChange={this.props.search}
+                  ></input>
+                </div>
+              </div>
+              <div className="cart">
+                <div className="cart-info">
+                  <table>
+                    <tbody>
+                      <tr>
+                        <td>Items</td>
+                        <td>:</td>
+                        <td>{itemCount}</td>
+                      </tr>
+                      <tr>
+                        <td>Total</td>
+                        <td>:</td>
+                        <td>{totalPrice}</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+                <a className="cart-icon" href="#" onClick={this.handleCartOpen}>
+                  <img
+                    src="https://raw.githubusercontent.com/itprofessionalsfrontend/shop/master/assets/bag.png"
+                    alt="Shopping Cart"
+                  ></img>
+                </a>
+                <div
+                  ref={this.cartRef}
+                  className={
+                    this.state.isCartOpen
+                      ? "cart-preview active"
+                      : "cart-preview"
+                  }
+                >
+                  <ul className="cart-items">
+                    {cartProducts.map((product) => {
+                      return (
+                        <li className="cart-item" key={product.id}>
+                          <img
+                            className="product-image"
+                            alt="Product"
+                            src={product.image}
+                          ></img>
+                          <div className="product-info">
+                            <p className="product-name">{product.name}</p>
+                            <p className="product-price">{product.price}</p>
+                          </div>
+                          <div className="product-total">
+                            <p className="quantity">{product.quantity}</p>
+                            <p className="amount">
+                              {product.price * product.quantity}
+                            </p>
+                          </div>
+                          <a
+                            className="product-remove"
+                            onClick={() => removeProduct(product.id)}
+                          >
+                            x
+                          </a>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="cart">
-            <div className="cart-info">
-              <table>
-                <tbody>
-                  <tr>
-                    <td>Items</td>
-                    <td>:</td>
-                    <td>{this.props.productCount}</td>
-                  </tr>
-                  <tr>
-                    <td>Total</td>
-                    <td>:</td>
-                    <td>{this.props.totalPrice}</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-            <a className="cart-icon" href="#" onClick={this.handleCartOpen}>
-              <img
-                src="https://raw.githubusercontent.com/itprofessionalsfrontend/shop/master/assets/bag.png"
-                alt="Shopping Cart"
-              ></img>
-            </a>
-            <div
-              ref={this.cartRef}
-              className={
-                this.state.isCartOpen ? "cart-preview active" : "cart-preview"
-              }
-            >
-              <ul className="cart-items">
-                <Scrollbars style={{ width: 360, height: 388 }}>
-                  {items}
-                </Scrollbars>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </header>
+          </header>
+        )}
+      </ProductContextConsumer>
     );
   }
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,12 @@
 import React, { Component } from "react";
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
-import axios from "axios";
 import Products from "./Products.jsx";
+import { ProductContextConsumer } from "../productContext.js";
 
 class Home extends Component {
   state = {
-    products: [],
     searchKeyword: "",
-    loading: true,
-    cartProducts: [],
-    itemCount: 0,
-    totalPrice: 0,
   };
 
   constructor(props) {
@@ -19,100 +14,12 @@ class Home extends Component {
     this.handleSearch = this.handleSearch.bind(this);
   }
 
-  getProducts() {
-    axios
-      .get(
-        "https://raw.githubusercontent.com/itprofessionalsfrontend/shop/master/products.json"
-      )
-      .then((response) => {
-        // setTimeout(
-        //   ()=>this.setState({ products: response.data, loading: false }),
-        //   10000
-        // );
-        this.setState({ products: response.data, loading: false });
-        console.log(response.data);
-      })
-      .catch(function (error) {
-        // handle error
-        console.log(error);
-      });
-  }
-
   handleSearch(event) {
     console.log("handle search called..");
     this.setState({ searchKeyword: event.target.value });
   }
 
-  componentDidMount() {
-    this.getProducts();
-  }
-
-  removeProduct = (id) => {
-    console.log("removeProduct", id);
-    //1.Method - filter
-    let filteredProducts = this.state.cartProducts.filter(
-      (item) => item.id !== id
-    );
-
-    this.setState({
-      cartProducts: filteredProducts,
-      itemCount: filteredProducts.length,
-      totalPrice: this.getTotalPrice(filteredProducts),
-    });
-
-    console.log("filteredList", filteredProducts);
-
-    //2.Method - splice
-  };
-
-  addToCart = (selectedProduct) => {
-    // console.log("Home addtocart called. " + selectedProduct.quantity);
-    let cartProductItems = this.state.cartProducts;
-    if (this.checkProduct(selectedProduct.id)) {
-      //todo:update
-      // console.log("already exist product start");
-      let productIndex = cartProductItems.findIndex(
-        (p) => p.id === selectedProduct.id
-      );
-      cartProductItems[productIndex].quantity += selectedProduct.quantity;
-      // console.log("already exist product end");
-    } else {
-      //add
-      cartProductItems.push(selectedProduct);
-    }
-    // console.log(cartProductItems);
-    this.setState({
-      cartProducts: cartProductItems,
-      itemCount: this.state.cartProducts.length,
-      totalPrice: this.getTotalPrice(cartProductItems),
-    });
-  };
-
-  checkProduct(productId) {
-    return this.state.cartProducts.some((product) => {
-      return product.id === productId;
-    });
-  }
-
-  getTotalPrice(cartProductList) {
-    console.log(cartProductList);
-    let priceArray = cartProductList.map((p) => p.price * p.quantity);
-    console.log(priceArray);
-
-    let result = priceArray.reduce(
-      (total, currentValue) => total + currentValue
-    );
-    return result;
-  }
-
   render() {
-    let productList = this.state.products.filter(
-      (item) =>
-        item.name
-          .toLowerCase()
-          .includes(this.state.searchKeyword.toLowerCase()) ||
-        !this.state.searchKeyword
-    );
     //MOVED INTO PRODUCTS COMPONENT
     // .map((product) => {
     //   return (
@@ -127,18 +34,27 @@ class Home extends Component {
 
     return (
       <>
-        <Header
-          search={this.handleSearch}
-          cartProducts={this.state.cartProducts}
-          productCount={this.state.itemCount}
-          totalPrice={this.state.totalPrice}
-          removeItem={this.removeProduct}
-        ></Header>
-        <Products
+        <Header search={this.handleSearch}></Header>
+        <ProductContextConsumer>
+          {({ products, loading, addToCart }) => (
+            <Products
+              loading={loading}
+              addToCart={addToCart}
+              productList={products.filter(
+                (item) =>
+                  item.name
+                    .toLowerCase()
+                    .includes(this.state.searchKeyword.toLowerCase()) ||
+                  !this.state.searchKeyword
+              )}
+            ></Products>
+          )}
+        </ProductContextConsumer>
+        {/* <Products
           productList={productList}
           loading={this.state.loading}
           addToCart={this.addToCart}
-        ></Products>
+        ></Products> */}
         <Footer></Footer>
       </>
     );
