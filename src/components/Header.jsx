c2c4b9bec5fb7bde88175e6ded88651f64590a76
MUSTAFA TOPÇU <mustafatopcu54@gmail.com>
2020-05-11 20:05:23 +0100

    useEffect and useRef

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,147 +1,122 @@
-import React, { Component } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { ProductContextConsumer } from "../productContext.js";
 
-class Header extends Component {
-  state = {
-    isCartOpen: false,
+const Header = (props) => {
+  const [isCartOpen, setIsCartOpen] = useState(false);
+
+  const cartRef = useRef();
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside, true);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside, true);
+    };
+  }, []);
+
+  const handleCartOpen = () => {
+    console.log("handleCartOpen");
+    setIsCartOpen(!isCartOpen);
   };
 
-  constructor(props) {
-    super(props);
-    this.handleCartOpen = this.handleCartOpen.bind(this);
-    this.cartRef = React.createRef();
-  }
 
-  handleCartOpen() {
-    console.log("handleCartOpen");
-    this.setState({
-      isCartOpen: !this.state.isCartOpen,
-    });
-  }
-
-  componentDidMount() {
-    document.addEventListener(
-      "click",
-      this.handleClickOutside.bind(this),
-      true
-    );
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener(
-      "click",
-      this.handleClickOutside.bind(this),
-      true
-    );
-  }
-
-  handleClickOutside(event) {
+  const handleClickOutside = (event) => {
     console.log(event.target);
-    let element = this.cartRef.current;
+    let element = cartRef.current;
 
     if (element.classList.contains("active")) {
       if (!element.contains(event.target)) {
-        this.setState({
-          isCartOpen: false,
-        });
+        setIsCartOpen(false);
         event.stopPropagation();
       }
     }
-  }
+  };
 
-  render() {
-    // console.log("header --->" + this.props.cartProducts.length);
-
-    return (
-      <ProductContextConsumer>
-        {({ itemCount, totalPrice, cartProducts, removeProduct }) => (
-          <header>
-            <div className="container">
-              <div className="brand">
-                <img
-                  className="logo"
-                  alt="Veggie Logo"
-                  src="https://raw.githubusercontent.com/itprofessionalsfrontend/shop/master/assets/veggie.png"
-                ></img>
-              </div>
-              <div className="search">
-                <div className="search-form">
-                  <input
-                    type="search"
-                    className="search-keyword"
-                    placeholder="Search vegetables and fruits..."
-                    onChange={this.props.search}
-                  ></input>
-                </div>
-              </div>
-              <div className="cart">
-                <div className="cart-info">
-                  <table>
-                    <tbody>
-                      <tr>
-                        <td>Items</td>
-                        <td>:</td>
-                        <td>{itemCount}</td>
-                      </tr>
-                      <tr>
-                        <td>Total</td>
-                        <td>:</td>
-                        <td>{totalPrice}</td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-                <a className="cart-icon" href="#" onClick={this.handleCartOpen}>
-                  <img
-                    src="https://raw.githubusercontent.com/itprofessionalsfrontend/shop/master/assets/bag.png"
-                    alt="Shopping Cart"
-                  ></img>
-                </a>
-                <div
-                  ref={this.cartRef}
-                  className={
-                    this.state.isCartOpen
-                      ? "cart-preview active"
-                      : "cart-preview"
-                  }
-                >
-                  <ul className="cart-items">
-                    {cartProducts.map((product) => {
-                      return (
-                        <li className="cart-item" key={product.id}>
-                          <img
-                            className="product-image"
-                            alt="Product"
-                            src={product.image}
-                          ></img>
-                          <div className="product-info">
-                            <p className="product-name">{product.name}</p>
-                            <p className="product-price">{product.price}</p>
-                          </div>
-                          <div className="product-total">
-                            <p className="quantity">{product.quantity}</p>
-                            <p className="amount">
-                              {product.price * product.quantity}
-                            </p>
-                          </div>
-                          <a
-                            className="product-remove"
-                            onClick={() => removeProduct(product.id)}
-                          >
-                            x
-                          </a>
-                        </li>
-                      );
-                    })}
-                  </ul>
-                </div>
+  return (
+    <ProductContextConsumer>
+      {({ itemCount, totalPrice, cartProducts, removeProduct }) => (
+        <header>
+          <div className="container">
+            <div className="brand">
+              <img
+                className="logo"
+                alt="Veggie Logo"
+                src="https://raw.githubusercontent.com/itprofessionalsfrontend/shop/master/assets/veggie.png"
+              ></img>
+            </div>
+            <div className="search">
+              <div className="search-form">
+                <input
+                  type="search"
+                  className="search-keyword"
+                  placeholder="Search vegetables and fruits..."
+                  onChange={props.search}
+                ></input>
               </div>
             </div>
-          </header>
-        )}
-      </ProductContextConsumer>
-    );
-  }
-}
+            <div className="cart">
+              <div className="cart-info">
+                <table>
+                  <tbody>
+                    <tr>
+                      <td>Items</td>
+                      <td>:</td>
+                      <td>{itemCount}</td>
+                    </tr>
+                    <tr>
+                      <td>Total</td>
+                      <td>:</td>
+                      <td>{totalPrice}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <a className="cart-icon" href="#" onClick={handleCartOpen}>
+                <img
+                  src="https://raw.githubusercontent.com/itprofessionalsfrontend/shop/master/assets/bag.png"
+                  alt="Shopping Cart"
+                ></img>
+              </a>
+              <div
+                ref={cartRef}
+                className={isCartOpen ? "cart-preview active" : "cart-preview"}
+              >
+                <ul className="cart-items">
+                  {cartProducts.map((product) => {
+                    return (
+                      <li className="cart-item" key={product.id}>
+                        <img
+                          className="product-image"
+                          alt="Product"
+                          src={product.image}
+                        ></img>
+                        <div className="product-info">
+                          <p className="product-name">{product.name}</p>
+                          <p className="product-price">{product.price}</p>
+                        </div>
+                        <div className="product-total">
+                          <p className="quantity">{product.quantity}</p>
+                          <p className="amount">
+                            {product.price * product.quantity}
+                          </p>
+                        </div>
+                        <a
+                          className="product-remove"
+                          onClick={() => removeProduct(product.id)}
+                        >
+                          x
+                        </a>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </header>
+      )}
+    </ProductContextConsumer>
+  );
+};
 
 export default Header;
